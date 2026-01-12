613cfbe781bc7d842ae02a9e2386274ccce60120
Rahinmon2903 <rahin2903@gmail.com>
2026-01-12 10:32:28 +0530

    protected Route updated

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const ProtectedRoute = ({ children, roles }) => {
+  const auth = JSON.parse(localStorage.getItem("auth"));
+
+  if (!auth) {
+    return <Navigate to="/login" />;
+  }
+
+  if (roles && !roles.includes(auth?.user?.role)) {
+    return <Navigate to="/unauthorized" />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
