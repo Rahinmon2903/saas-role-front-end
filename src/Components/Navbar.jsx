43e0e87ebd938ed525e85565d54d0f7913131873
Rahinmon2903 <rahin2903@gmail.com>
2026-01-12 10:38:35 +0530

    basic navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("auth");
+    navigate("/login"); 
+  };
+
+  return (
+    <div className="h-14 bg-gray-800 text-white flex justify-between px-4 items-center">
+      <h1 className="font-semibold">SaaS Dashboard</h1>
+      <div>
+        {auth?.user?.name}
+        <button onClick={logout} className="ml-4 text-red-400">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
+
