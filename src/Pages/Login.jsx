976afeb2698a617b5691d7d081f5222d0f6cc932
Rahinmon2903 <rahin2903@gmail.com>
2026-01-12 11:03:44 +0530

    login updated

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useState } from "react";
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate=useNavigate();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+            const res = await api.post("/auth/login", { email, password });
+    localStorage.setItem("auth", JSON.stringify(res.data));
+    navigate("/");
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+
+  
+
+    
+  };
+
+  return (
+    <form onSubmit={submit} className="max-w-sm mx-auto mt-20">
+      <input placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+      <input type="password" value={password} placeholder="Password"
+        onChange={(e)=>setPassword(e.target.value)} />
+      <button>Login</button>
+    </form>
+  );
 };
 
 export default Login;
